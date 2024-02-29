2989a8dacbd3e9189be73d208827c7c5b0839c7e
Joseph Thomas <josephdthomas@google.com>
2024-02-29 06:25:40 +0000

    Add bugbash example

--- examples/im_cloudbuild_workspace_github/main.tf
+++ examples/im_cloudbuild_workspace_github/main.tf
@@ -21,11 +21,42 @@ module "im_workspace" {
   deployment_id = "im-example-github-deployment"
 
   tf_repo_type           = "GITHUB"
-  im_deployment_repo_uri = "https://github.com/im-goose/infra-manager-git-example.git"
-  im_deployment_ref      = "main"
-  im_tf_variables        = "project_id=${var.project_id}"
+  im_deployment_repo_uri = "https://github.com/josephdt12/terraform-google-bootstrap.git"
+  im_deployment_repo_dir = "examples/im_cloudbuild_workspace_github"
+  im_deployment_ref      = "bugbash-example"
   infra_manager_sa_roles = ["roles/compute.networkAdmin"]
 
-  github_app_installation_id   = "47590865"
+  github_app_installation_id   = "47236181"
   github_personal_access_token = var.im_github_pat_secret
+}
+
+module "bucket" {
+  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+  version = "~> 5.0"
+
+  name       = "im-${var.project_id}-example-bucket"
+  project_id = var.project_id
+  location   = "us"
+
+  lifecycle_rules = [{
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      age            = 365
+      with_state     = "ANY"
+      matches_prefix = var.project_id
+    }
+  }]
+
+  custom_placement_config = {
+    data_locations : ["US-EAST4", "US-WEST1"]
+  }
+
+  iam_members = [{
+    role   = "roles/storage.objectViewer"
+    member = "group:test-gcp-ops@test.blueprints.joonix.net"
+  }]
+
+  autoclass = true
 }
